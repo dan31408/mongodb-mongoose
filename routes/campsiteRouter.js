45bba697f5dff1f804d22c0a87a509ccac50a137
Dan Beto <dan.beto31408@gmail.com>
2020-03-16 12:21:02 -0500

    Express Generator REST API

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const promotionRouter = require('/routes/promotionRouter');
+const campsiteRouter = express.Router();
+const partnerRouter = require('/routes/partnerRouter');
+
+campsiteRouter.use(bodyParser.json());
+promotionRouter.use(bodyParser.json());
+partnerRouter.use(bodyParser.json());
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+//Week 1 assignment method
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+})
+
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+module.exports = campsiteRouter;
