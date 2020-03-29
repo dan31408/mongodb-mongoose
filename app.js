7afd2c16627d6e91d937231e101b31611c98df69
Dan Beto <dan.beto31408@gmail.com>
2020-03-29 11:57:51 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,18 +34,22 @@ connect.then(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use("/", indexRouter);
+app.use("/", usersRouter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-09876-54321"));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUnitialized: false,
-  resave: false,
-  store: newFileStore()
-}));
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUnitialized: false,
+    resave: false,
+    store: newFileStore()
+  })
+);
 
 // this is where we'll add authentication
 
@@ -53,29 +57,11 @@ function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      req.session.user = 'admin';
-      return next(); // authorized
-    } else {
-      const err = new Error("You are not authorized!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    const err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === "authenticated") {
       return next();
     } else {
       const err = new Error("You are not authorized!");
